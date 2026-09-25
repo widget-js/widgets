cee35f74a0653e91887005d8f019f28fc8252de9
neo <rtugeek@gmail.com>
2026-09-25 11:34:39 +0800

    chore: update GitHub Actions and package dependencies to latest versions

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
     ],
     resolve: {
       alias: {
-        '@widget-js/core': path.resolve(__dirname, '../@widget-js/core/dist/index.js'),
+        // '@widget-js/core': path.resolve(__dirname, '../@widget-js/core/dist/index.js'),
         '@': path.resolve(__dirname, './src'),
       },
     },
